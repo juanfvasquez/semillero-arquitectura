6a78237183fd2348a4fcf8b6ea89a4688035414e
Juan <juanfvasquez267@gmail.com>
2021-11-09 18:43:39 -0500

    Operadores Streams

--- src/Streams.java
+++ src/Streams.java
@@ -1,17 +1,19 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 public class Streams {
-    static List<String> cadenas = Arrays.asList("Cadena 1", "Cadena 2", "Cadena 123", "Cadena 12133");
+    static List<String> cadenas = Arrays.asList("Cadena 1", "Cadena 2", "Cadena 123", "Cadena 12133", " ");
     static List<Persona> personas = Arrays.asList();
 
     public static void main(String[] args) {
 //        usoForEach();
 //        usoFilter();
 //        usoMap();
-        usoReduce();
+//        usoReduce();
+        usoMatch();
     }
 
     public static void usoForEach() {
@@ -53,7 +55,37 @@ public class Streams {
 //        for (String s: cadenas) {
 //            acum += s.length();
 //        }
-        Integer total = cadenas.stream().map(s -> s.length()).reduce((acum, len) -> acum + len);
+        Integer total = cadenas.stream().map(s -> s.length()).reduce((acum, len) -> acum + len).get();
+        System.out.println(total);
+        cadenas.stream().map(s -> s.length()).reduce((acum, len) -> acum + len).ifPresent(System.out::println);
+
+        cadenas.stream().reduce((s1, s2) -> {
+            //
+            return s1 + ", " + s2;
+        }).ifPresent(System.out::println);
+
+        Optional<String> op = Optional.empty();
+        op.ifPresent(System.out::println);
+        System.out.println(op.orElse("Valor por defecto"));
+        op.orElseGet(() -> "Otro valor por defecto");
+    }
+
+    public static void usoMatch() {
+        boolean hayCadenaVacia = cadenas.stream().anyMatch(s -> s.length() == 0);
+        System.out.println(hayCadenaVacia);
+
+        boolean permitido = cadenas.stream().noneMatch(s -> s.length() == 0);
+        System.out.println(permitido ? "Permitido" : "No permitido");
+
+        permitido = cadenas.stream().allMatch(s -> s.length() > 0);
+        System.out.println(permitido ? "Permitido" : "No permitido");
+
+//        cadenas.stream().map(s -> new Persona(s, s.length()))
+        boolean hayMenores = cadenas.stream()
+                .map(Persona::new)
+                .filter(p -> p.nombre.contains("123"))
+                .noneMatch(p -> p.edad < 10);
+        System.out.println(hayMenores ? "Hay menores" : "No hay menores");
     }
 }
 
@@ -64,6 +96,11 @@ class Persona {
     public Persona(String nombre, int edad) {
         this.nombre = nombre;
         this.edad = edad;
+    }
+
+    public Persona(String s) {
+        this.nombre = s;
+        this.edad = s.length();
     }
 
     @Override
